b6a362458a51b27b20c267974dde3c85cc5a6a43
iamarasekera <iamarasekera.dev@gmail.com>
2024-11-24 01:32:42 +1030

    style: Enhance UI to display movie poster image and watchlist items

--- src/components/WatchList.tsx
+++ src/components/WatchList.tsx
@@ -3,7 +3,7 @@
  * This componentm manages user's saved or watched movies
 */
 import React, { FC }  from 'react';
-import { Box, Typography, List, ListItem, ListItemText } from '@mui/material';
+import { Paper, Typography, List, ListItem, ListItemText, ListItemAvatar, Avatar } from '@mui/material';
 import { Movie } from '../types';
 
 // Define the types for the props WatchList component will receive
@@ -12,20 +12,40 @@ interface WatchListProps {
 }
 
 // Functional component for the WatchList
-const WatchList: FC<WatchListProps> = ({ watchlist }) => (
-<Box>
-  {/* Display the text Watchlist */}
-    <Typography variant="h6">Watchlist</Typography>
-    {/* List of movies in the watchlist */}
-    <List>
-      {watchlist.map((movie) => (
-        <ListItem key={movie.imdbID}>
-           {/* Display the movie title and year of the movie added */}
-          <ListItemText primary={movie.Title} secondary={movie.Year} />
-        </ListItem>
-      ))}
-    </List>
-  </Box>
-);
+const WatchList: FC<WatchListProps> = ({ watchlist }) => {
+  return (
+    <Paper sx={{ mt: 2, p: 2 }}>
+      {/* Display the Watchlist Title */}
+      <Typography variant="h6" gutterBottom>
+        My Watchlist ({watchlist.length})
+      </Typography>
+      {/* Check if the watchlist is empty */}
+      {watchlist.length === 0 ? (
+        //If the watchlist is empty display "WatchList is empty" 
+        <Typography variant="body2" color="textSecondary">
+          Your watchlist is empty. Add movies to your watchlist to see them here.
+        </Typography>
+      ) : (
+        /*Display the list of movies in the watchlist */
+        <List sx={{ maxHeight: 300, overflow: 'auto' }}>
+          {watchlist.map((movie) => (
+            <ListItem key={movie.imdbID}>
+               {/* Display the movie's poster image */}
+              <ListItemAvatar>
+                <Avatar
+                  alt={movie.Title}
+                  src={movie.Poster !== 'N/A' ? movie.Poster : '/placeholder.png'}
+                  variant="rounded"
+                />
+              </ListItemAvatar>
+              {/* Display the movie title and year of the movie added */}
+              <ListItemText primary={movie.Title} secondary={movie.Year} />
+            </ListItem>
+          ))}
+        </List>
+      )}
+    </Paper>
+  );
+};
 
 export default WatchList;
