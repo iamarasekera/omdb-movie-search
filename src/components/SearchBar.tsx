9280b02e018f009ddabc72fa70338900352c66a2
iamarasekera <iamarasekera.dev@gmail.com>
2024-11-25 12:07:27 +1030

    style: Remove exsiting search button from SearchBar to incoperate the design

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -7,7 +7,6 @@ import {
     Box,
     TextField,
     InputAdornment,
-    Button,
     Select,
     MenuItem,
     FormControl,
@@ -37,85 +36,76 @@ const SearchBar: FC<SearchBarProps> = ({
     setYear,
     type,
     setType
-  }) => (
+}) => (
     //Search Bar Container
     <Box display="flex" gap={2} mb={3} sx={{
       backgroundColor: '#727272', // Set background color of the to gray
-      padding: 2,
-      borderRadius: 1, 
+        padding: 2,
+        borderRadius: 1,
       width: '100%' // To ensure it spans the full width of the container
     }}>
      {/* Input field for searching movies */}
-    <TextField
-      placeholder="Search Movies"
-      variant="standard"       
-      fullWidth
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-      onKeyPress={(e) => {
-        if (e.key === 'Enter') {
-          onSearch();
-        }
-      }}
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchIcon sx={{ color: 'white' }} />
-          </InputAdornment>
-        ),
+        <TextField
+            placeholder="Search Movies"
+            variant="standard"       
+            fullWidth
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyPress={(e) => {
+                if (e.key === 'Enter' && !loading) {
+                    onSearch();
+                }
+            }}
+            InputProps={{
+                startAdornment: (
+                    <InputAdornment position="start">
+                        <SearchIcon sx={{ color: 'white' }} />
+                    </InputAdornment>
+                ),
         disableUnderline: true, // Disabled underline for borderless effect
-      }}
-      sx={{
-        input: {
+            }}
+            sx={{
+                input: {
           color: 'white', // Input text color
           padding: '20px 0', // Adjust padding for vertical centering
-        },
-        '& .MuiInputBase-root': {
-          display: 'flex',
+                },
+                '& .MuiInputBase-root': {
+                    display: 'flex',
           alignItems: 'center', // Vertically center icon and text
           backgroundColor: 'transparent', // Set transparent background
-        },
-        '& .MuiInputBase-input::placeholder': {
+                },
+                '& .MuiInputBase-input::placeholder': {
           color: 'white', // Placeholder text color
           opacity: 1, // Ensure full opacity
-        },
-        '& .MuiInputAdornment-root': {
+                },
+                '& .MuiInputAdornment-root': {
           marginTop: '0', // To prevent misalignment
-        },
-      }}
-    />
+                },
+            }}
+        />
     {/* Input field for selecting the year */}
-    <TextField
-      label="Year"
-      variant="outlined"
-      value={year}
-      onChange={(e) => setYear(e.target.value)}
-      sx={{ width: 100 }}
-    />
+        <TextField
+            label="Year"
+            variant="outlined"
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+            sx={{ width: 100 }}
+        />
     {/* Dropdown for selecting the type of content */}
-    <FormControl sx={{ minWidth: 120 }}>
-      <InputLabel>Type</InputLabel>
-      <Select
-        value={type}
-        label="Type"
-        onChange={(e) => setType(e.target.value as typeof type)}
-      >
-        <MenuItem value="">All</MenuItem>
-        <MenuItem value="movie">Movie</MenuItem>
-        <MenuItem value="series">Series</MenuItem>
-        <MenuItem value="episode">Episode</MenuItem>
-      </Select>
-    </FormControl>
-     {/* Search button to trigger the search */}
-    <Button 
-      variant="contained" 
-      color="primary" 
-      onClick={onSearch}
-      disabled={loading}
-    >
-      Search
-    </Button>
-  </Box>
+        <FormControl sx={{ minWidth: 120 }}>
+            <InputLabel>Type</InputLabel>
+            <Select
+                value={type}
+                label="Type"
+                onChange={(e) => setType(e.target.value as typeof type)}
+            >
+                <MenuItem value="">All</MenuItem>
+                <MenuItem value="movie">Movie</MenuItem>
+                <MenuItem value="series">Series</MenuItem>
+                <MenuItem value="episode">Episode</MenuItem>
+            </Select>
+        </FormControl>
+    </Box>
 );
 
 export default SearchBar;
